c46d9525c1b47f4d24f64c51aff82eba01bc15ec
rightgem <62364511+rightgem@users.noreply.github.com>
2021-09-26 02:59:41 +0900

    04-2 까지 해봤다

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Potato from './Potato';
+
+function Tomato(props) {
+  return <h3> Tomato in App component, prop : {props.fav} </h3>
+}
+
+function MapFunc({name, dat}){
+  //구조 분해 할당 사용
+  return (
+    <div>
+      <h1> 구조분해할당 변수 : {name}</h1>
+      <p> 이미지 태그는 img src='링크'로 쓰는거 잊지말기... {dat}</p>
+    </div>
+  )
+}
+
+const mapFunctionTest = [
+  {
+    name: 'data1',
+    image : 'asd'
+  },
+  {
+    name: 'data2',
+    image : 'sd'
+  },
+  {
+    name: 'data3',
+    image : 'd'
+  },
+]
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( 
+    <div> 
+      <h1>Hello World</h1>
+      {/* <Potato /> */}
+      {/* <Tomato fav="ketchup" /> */}
+      {mapFunctionTest.map(i => (<MapFunc name={i.name} dat={i.image} />) )}
     </div>
   );
 }
---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Potato() {
+    return (
+        <h3>나는 감자다</h3>
+    )
+}
+
+export default Potato;
