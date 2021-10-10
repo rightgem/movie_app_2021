2f11d9847a81acbcf91f1775ff4c8f70fdad2a3f
rightgem <62364511+rightgem@users.noreply.github.com>
2021-10-10 23:59:58 +0900

    ddd
    리액트 왜...이렇게..어려워

--- src/Potato.js
+++ src/Potato.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Potato() {
     return (
-        <h3>나는 감자다</h3>
+        <div>
+            <h3>나는 감자다</h3>
+            <h1>리액트 너무 어렵다....<h1/>
+        <div/>
     )
 }
 
