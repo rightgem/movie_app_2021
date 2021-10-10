7f09b2473fdfa55c003b56b24556c8612c6fb87b
rightgem <62364511+rightgem@users.noreply.github.com>
2021-10-11 00:00:41 +0900

    confirm close tag

--- src/Potato.js
+++ src/Potato.js
@@ -4,8 +4,8 @@ function Potato() {
     return (
         <div>
             <h3>나는 감자다</h3>
-            <h1>리액트 너무 어렵다....<h1/>
-        <div/>
+            <h1>리액트 너무 어렵다....</h1>
+        </div>
     )
 }
 
